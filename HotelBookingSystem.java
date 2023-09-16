ec8f73eccda9d78b4226d4befaa316f8ca7b2222
akbar fahri <144417878+akbarfahri@users.noreply.github.com>
2023-09-16 11:32:07 +0700

    second commend

--- HotelBookingSystem.java
+++ HotelBookingSystem.java
@@ -14,7 +14,21 @@ public class HotelBookingSystem {
         System.out.println("Selamat datang di sistem booking hotel!");
         System.out.print("Masukkan nama pemesan: ");
         namaPemesan = input.nextLine();
-       
+       System.out.print("Masukkan tipe kamar (superior/president): ");
+        tipeKamar = input.nextLine();
+        System.out.print("Masukkan alat transaksi (cc/cash/dll.): ");
+        alatTransaksi = input.nextLine();
+        System.out.print("Masukkan jumlah malam menginap: ");
+        jumlahMalam = input.nextInt();
+        System.out.print("Masukkan pembayaran: ");
+        pembayaran = input.nextDouble();
+
+         // Proses perhitungan total biaya
+         double totalBiaya = jumlahMalam * hargaPerMalam;
+        
+         // Output detail booking
+         System.out.println("\nDetail Booking:");
+         System.out.println("Nama Pemesan: " + namaPemesan);
  
  
  
