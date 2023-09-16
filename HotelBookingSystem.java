581361db73e123fd2b42fc6957ba965d5ba61c93
aslamrosul <143428821+aslamrosul@users.noreply.github.com>
2023-09-16 11:10:20 +0700

    "first commit"

---
+++ HotelBookingSystem.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class HotelBookingSystem {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi variabel input
+        Scanner input = new Scanner(System.in);
+        String namaPemesan;
+        String tipeKamar;
+        String alatTransaksi;
+        double pembayaran;
+        int jumlahMalam;
+        double hargaPerMalam = 750000; // Harga kamar per malam
+        
+        // Input data dari pengguna
+        System.out.println("Selamat datang di sistem booking hotel!");
+        System.out.print("Masukkan nama pemesan: ");
+        namaPemesan = input.nextLine();
+       
+ 
+ 
+ 
+    }
