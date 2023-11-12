3979d290835d2e98bc6b53b14b5d3d6894ccd972
adanisalsabila <143781493+adanisalsabila@users.noreply.github.com>
2023-11-12 10:34:13 +0700

    Update FiturLogIn.java

--- FiturLogIn.java
+++ FiturLogIn.java
@@ -7,7 +7,14 @@ public class FiturLogIn {
         int pilihMenu;
         int[] superiorRoomsAvailable = new int[10];
         int[] presidentRoomsAvailable = new int[10];
-
+        int[] jenisKamar = new int[2];
+        boolean pesanKamar=true;
+        String[] tipeKamar = { "Superior", "Deluxe", "President Suite" };
+        double[] hargaPerMalam = { 750000, 500000, 2000000 };
+        String[] layananTambahan = { "Extra Bed", "Makanan", null };
+        double[] hargaLayanan = { 50000, 100000, 0 };
+        int bookRoom;
+        
         System.out.println("=======================================");
         System.out.println("Selamat Datang di Ijen Suites Hotel!");
         System.out.println("=======================================");
@@ -55,55 +62,57 @@ public class FiturLogIn {
                     System.out.println("^ Private Bathroom");
                     System.out.println("^ Air Conditioning");
                     System.out.println("=======================================");
-                    System.out.println("Masukkan jenis kamar yang ingin Anda cek :"+jenisKamar);
- 
-                    String jenisKamar = sc.next();
+                
+                    System.out.println("Masukkan jenis kamar yang ingin Anda cek :");
 
-                   
-                    if (jenisKamar.equals("1") && hasAvailableRoom(superiorRoomsAvailable)) {
-                        System.out.println("Superior Deluxe Room tersedia. Anda bisa memesannya!");
-                        bookRoom(superiorRoomsAvailable);
-                    } else if (jenisKamar.equals("2") && hasAvailableRoom(presidentRoomsAvailable)) {
-                        System.out.println("Devaxa President Room tersedia. Anda bisa memesannya!");
-                        bookRoom(presidentRoomsAvailable);
+                    int selectedRoomType = sc.nextInt();
+                    if (selectedRoomType == 1 && hasAvailableRoom(superiorRoomsAvailable)) {
+                    System.out.println("Superior Deluxe Room tersedia. Anda bisa memesannya!");
+                    bookRoom(superiorRoomsAvailable);
+                    } else if (selectedRoomType == 2 && hasAvailableRoom(presidentRoomsAvailable)) {
+                    System.out.println("Devaxa President Room tersedia. Anda bisa memesannya!");
+                    bookRoom(presidentRoomsAvailable);
                     } else {
-                        System.out.println("Maaf, kamar yang Anda pilih tidak tersedia. Silahkan pilih kamar yang lain.");
-                    }
-
-                    displayRemainingRooms(superiorRoomsAvailable, "Superior Deluxe");
-                    displayRemainingRooms(presidentRoomsAvailable, "Devaxa President");
-
-                    System.out.println("Do you want to continue checking availability? (Y/N): ");
-                } while (sc.next().equalsIgnoreCase("Y"));
+                    System.out.println("Maaf, kamar yang Anda pilih tidak tersedia. Silahkan pilih kamar yang lain.");
+}
+                break; 
+            case 2:
+            do {
+                    System.out.println("Pesan Kamar");
+                    System.out.println("Masukkan nama Anda : ");
+                    String namaPemesan = sc.next();
+                    System.out.println("Masukkan jenis kamar yang Anda pilih : ");
+                    jenisKamar = sc.nextLine();
+                    System.out.println("Masukkan jumlah kamar yang Anda pesan : ");
+                    int jumlahKamar = sc.nextInt();
+                    System.out.println("Masukkan jumlah malam Anda menginap : ");
+                    int jumlahMalam = sc.nextInt();
+            while (pesanKamar) {
+                for (int i = 0; i < jenisKamar.length; i++) {
+                    System.out.println(i + 1 + ". " + jenisKamar[i] + " - $" + hargaPerMalam[i]);
+                }
+            }            
                 break;
-        }
-    }
-
-    private static boolean hasAvailableRoom(int[] roomsAvailable) {
-        for (int room : roomsAvailable) {
-            if (room == 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static void bookRoom(int[] roomsAvailable) {
-        for (int i = 0; i < roomsAvailable.length; i++) {
-            if (roomsAvailable[i] == 0) {
-                roomsAvailable[i] = 1;
+            case 3:
+                
                 break;
-            }
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+            case 6:
+                
+                break;
+            case 7:
+                
+                break;
+            default:
+                System.out.println("Pilihan tidak valid");
         }
+        
     }
-
-    private static void displayRemainingRooms(int[] roomsAvailable, String roomType) {
-        int remainingRooms = 0;
-        for (int room : roomsAvailable) {
-            if (room == 0) {
-                remainingRooms++;
-            }
-        }
-        System.out.println("Remaining " + roomType + " Rooms: " + remainingRooms);
+}
     }
 }
