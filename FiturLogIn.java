02c87921860ba2ef0e997ed9b54d145938b1a565
adanisalsabila <143781493+adanisalsabila@users.noreply.github.com>
2023-11-21 21:31:25 +0700

    Update FiturLogIn.java

--- FiturLogIn.java
+++ FiturLogIn.java
@@ -101,7 +101,34 @@ public class FiturLogIn {
                 
                 break;
             case 5:
-                
+                String alamatHotel;
+        int cekLokasiHotel;
+
+        System.out.println("Selamat datang di Hotel XYZ!");
+        System.out.println("Silakan pilih opsi:");
+        System.out.println("1. Cek Lokasi Hotel");
+        
+        int option = sc.nextInt();
+
+        switch (option) {
+            case 1:
+            System.out.println("===============================================================================");
+            System.out.println("Jalan Ijen Nirwana Raya Blok A No.16, Malang Center, Malang, Indonesia, 65116 ");
+            System.out.println("===============================================================================");
+                break;
+            default:
+                System.out.println("Opsi tidak valid.");
+        }
+    }
+
+    private static void checkHotelLocation(double latitude, double longitude) {
+        System.out.println("Lokasi Hotel:");
+        System.out.println("Latitude: " + latitude);
+        System.out.println("Longitude: " + longitude);
+    }
+}
+
+
                 break;
             case 6:
                 
